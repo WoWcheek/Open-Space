e12ee804054b4018af29fbc5c024e36d9ac8fe1a
Savchuk Volodymyr <savchuk.v03@gmail.com>
2024-07-03 13:20:50 +0300

    init

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+    return (
+        <main>
+            <div>PositionTile</div>
+            <div>DateTimeTile</div>
+            <div>Map</div>
+            <div>People</div>
+        </main>
+    );
+}
+
+export default App;
