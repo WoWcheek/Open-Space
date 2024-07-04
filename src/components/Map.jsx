0eebcd3d3a221c51e0adeb926736bc7f571ca304
Savchuk Volodymyr <savchuk.v03@gmail.com>
2024-07-04 12:56:56 +0300

    style: make page adaptive

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -23,29 +23,27 @@ function Map() {
     }, [issPosition]);
 
     return (
-        <div>
-            <div className={styles.mapContainer}>
-                {issPosition && (
-                    <MapContainer
-                        center={mapPosition}
-                        zoom={5}
-                        scrollWheelZoom={true}
-                        className={styles.map}
-                    >
-                        <TileLayer
-                            attribution='&copy; <a href="https://carto.com/">carto.com</a> contributors'
-                            url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png"
-                        />
-                        {issPosition?.latitude && (
-                            <Marker position={mapPosition} icon={customMarker}>
-                                <Popup>
-                                    <span>ISS current position</span>
-                                </Popup>
-                            </Marker>
-                        )}
-                    </MapContainer>
-                )}
-            </div>
+        <div className={styles.mapContainer}>
+            {issPosition && (
+                <MapContainer
+                    center={mapPosition}
+                    zoom={5}
+                    scrollWheelZoom={true}
+                    className={styles.map}
+                >
+                    <TileLayer
+                        attribution='&copy; <a href="https://carto.com/">carto.com</a> contributors'
+                        url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png"
+                    />
+                    {issPosition?.latitude && (
+                        <Marker position={mapPosition} icon={customMarker}>
+                            <Popup>
+                                <span>ISS current position</span>
+                            </Popup>
+                        </Marker>
+                    )}
+                </MapContainer>
+            )}
         </div>
     );
 }
